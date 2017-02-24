401b4443bac2c96f5cb021e7cb31597ef6cd42da
Nikolay <kolyan95@gmail.com>
2017-02-24 02:51:22 +0200

    changed bubble method

--- src/pro/sort/method/BubbleMethod.java
+++ src/pro/sort/method/BubbleMethod.java
@@ -9,9 +9,9 @@ public class BubbleMethod implements ISorterMethod {
 	}
 
 	public <T> void sort(T[] array, int start, int end, Comparator<T> comparator, int skip) {
-		System.out.println("sortingBubbles:"+(start + skip + 1)+" => "+end+"\n");
+		System.out.println("sortingBubbles:" + (start + skip + 1) + " => " + end + "\n");
 		for (int i = start + skip + 1; i < end; i++) {
-			for (int j = 1; j < (end - i); j++) {
+			for (int j = i + 1; j < (end - 1); j++) {
 				if (comparator.compare(array[i], array[j]) < 0) {
 					T temp = array[i];
 					array[i] = array[j];
